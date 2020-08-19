34c6da47ae08aa80d04153f388f12384003001cb
vagnerzelom <vagnerzelom@hotmail.com>
2020-08-18 22:16:01 -0300

    adicionar dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const {uuid,isUuid} = require('uuidv4');
 
@@ -89,7 +90,7 @@ app.delete("/transactions/:id", (request, response) => {
     return response.status(204).send();
   });
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, ()=> {
     console.log(`Server up and running on port ${port}`);
